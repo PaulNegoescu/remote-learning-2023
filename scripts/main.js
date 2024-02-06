c19b82b609a9022582771a90ff6b9790368d8a1e
Paul Negoescu <paul.negoescu@nagarro.com>
2024-02-06 15:00:05 +0200

    First JS course

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+'use strict';
+var nume = 'Paul';
+let varsta = 12;
+const conditie = true;
+let c = 42;
+
+function add(a, b) {
+  return a + b;
+}
+
+// varsta = varsta + 1;
+// varsta += 1;
+// varsta++;
+
+if (Number.isNaN(varsta)) {
+  console.log('Era true');
+} else {
+  console.log('era false');
+}
