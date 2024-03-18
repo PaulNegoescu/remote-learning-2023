05df5910c015171983905a60c807851574fb1008
Ana Seiculescu <anaseiculescu@gmail.com>
2024-03-18 17:50:55 +0200

    merging dom branch into ana-seiculescu branch

--- scripts/main.js
+++ scripts/main.js
@@ -1,61 +1,21 @@
-// Recap:
-// let, const variables
-// primitve values (number, string, boolean, bigint, undefined, symbol, null)
-// scope
-//   global, function/local, block, module
-// functions 
-//   declarations, expressions, arrow
-// objects
-//   classes, constructor functions
-//   shorthand property declaration, shorthand method declaration
-          // const a = 5;
-          // const b = 6;
-          // const o = {
-          //   a, 
-          //   b: b,
-          //   method() {
-          //     console.log('shorthand');
-          //   },
-          //   long: function() {
-          //     console.log('long');
-          //   }
-          // }
-//   this
-// arrays
-//   array methods: pop, push, slice, shift, unshift
-// ifs, fors, whiles, switches
-    // for(const prop in o) {
-              // shorthand conditional
-    //   console.log(typeof o[prop] === 'function' && o[prop]());
-    // }
-
-
-// TODO: array functional methods
-
-// DOM: Document Object Model
-
-
-// Cand dam clic pe butonul + vrem ca valoarea din output sa creasca cu 1
-//  valoare sa creasca cu 1: val += 1;
-//  cand se da click: .addEventListener('click', () => {})
-//  butonul +, output: document.querySelector('[data-...]');
-
-// Cand dam clic pe butonul - vrem ca valoarea din output sa scada cu 1
-
 (function () {
-  const incButton = document.querySelector('[data-counter-button=increment]');
+  // const incButton = document.querySelector('[data-counter-button=increment]');
+  const countButtonsArray = document.querySelectorAll('[data-counter-button]');
+  // console.log(countButtonsArray);
   const output = document.querySelector('[data-counter-output]');
   let count = 0;
   displayCount(count);
 
-  incButton.addEventListener('click', handleClick);
+  // incButton.addEventListener('click', handleClick);
+  countButtonsArray.forEach((button) => button.addEventListener('click', handleClick));
 
   function handleClick(e) {
-    count += 1;
-    displayCount(count);
+      // console.log(e);
+      e.target.closest('[data-counter-button=increment') ? count += 1 : count -= 1;
+      displayCount(count);
   }
 
   function displayCount(count) {
-    output.textContent = count;
+      output.textContent = count;
   }
 })();
